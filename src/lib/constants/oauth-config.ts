f6b81e728742be98551c6a6a3e8908a86c64f910
Rishi B <rishibalakrishnan@berkeley.edu>
2026-01-06 12:19:26 -0500

    change admin signin route

--- src/lib/constants/oauth-config.ts
+++ src/lib/constants/oauth-config.ts
@@ -28,16 +28,16 @@ export const BLUE_SKY_CLIENT_META_DATA: OAuthClientMetadataInput = {
 
 export const BLUE_SKY_ADMIN_CLIENT_META_DATA: OAuthClientMetadataInput = {
 	// Must be a URL that will expose admin client metadata
-	client_id: `${baseUrl}/admin/auth/client-metadata.json`,
+	client_id: `${baseUrl}/auth/admin/client-metadata.json`,
 	client_name: 'Safe Skies Admin',
 	client_uri: baseUrl,
-	redirect_uris: [`${baseUrl}/admin/auth/callback`],
+	redirect_uris: [`${baseUrl}/auth/admin/callback`],
 	grant_types: ['authorization_code', 'refresh_token'],
 	scope: 'atproto transition:generic',
 	response_types: ['code'],
 	application_type: 'web',
 	token_endpoint_auth_method: 'private_key_jwt',
 	token_endpoint_auth_signing_alg: 'ES256',
-	jwks_uri: `${baseUrl}/admin/auth/jwks.json`,
+	jwks_uri: `${baseUrl}/auth/admin/jwks.json`,
 	dpop_bound_access_tokens: true,
 }
